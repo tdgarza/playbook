4eed5f6f8f82fb32b158697c2f22ac38abb16dd6
Tomas Garza <99302916+tdgarza@users.noreply.github.com>
2022-04-21 17:48:34 -0500

    Agregando clase y su prueba de unidad

---
+++ weekly_mission_3/lives/marvel/test/Superhero.test.js
@@ -0,0 +1,9 @@
+const Superhero = require("./../app/Superhero")
+describe ("Unit Test for Superhero", ()=>{
+    test('Case 1: Get a Superhero', ()=>{
+        const ironman = new Superhero("Iron Man", "Tony Stark", "Robert Downey Jr.")
+        expect(ironman.name).toBe("Iron Man")
+        expect(ironman.actor).toBe("Robert Downey Jr.")
+        expect(ironman.heroName).toBe("Tony Stark")
+    });
+})
