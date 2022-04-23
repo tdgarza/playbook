01c4892dd581c7c51d0a4b0b663be1c24adbcd16
Tomas Garza <99302916+tdgarza@users.noreply.github.com>
2022-04-23 11:15:27 -0500

    Add files via upload

---
+++ weekly_mission_3/lives/twitter/app/models/User.js
@@ -0,0 +1,21 @@
+class User{
+    constructor(id, username, name, bio){
+        this.id = id
+        this.username = username
+        this.name = name
+        this.bio = bio
+        this.dateCreated = new Date()
+        this.lastUpdated = new Date()
+    }
+    get getUsername(){
+        return this.username
+
+    }
+    set setUsername(newUsername){
+        this.username = newUsername
+    }
+    get getBio(){
+        return this.bio
+      }
+ }
+module.exports = User
