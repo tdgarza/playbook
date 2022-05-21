1d9f92a2f0242721a7422fde3e607650ebb7816a
Tomas Garza <99302916+tdgarza@users.noreply.github.com>
2022-05-21 10:30:03 -0500

    Paso 1: Inicializando mi proyecto

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const fs = require("fs");
 const rawdata = fs.readFileSync("explorers.json");
 const explorers = JSON.parse(rawdata);
 
+console.log("Hey! i'm here!")
+//console.log(explorers)
+
 // Part 2: Get the quantity of explorers names in node
 const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
 //console.log(explorersInNode.length)
